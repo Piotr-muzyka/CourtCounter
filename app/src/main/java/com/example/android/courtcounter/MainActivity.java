7590c94be3128654febb905e5657aa51e3cbe820
Piotr Muzyka <piotrmuzyka@yahoo.com>
2017-03-22 18:22:04 +0100

    Added counters for Two teams and methods in order to add points to a particular team.

--- app/src/main/java/com/example/android/courtcounter/MainActivity.java
+++ app/src/main/java/com/example/android/courtcounter/MainActivity.java
@@ -2,12 +2,55 @@ package com.example.android.courtcounter;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
-
+    int scoreTeamA =0;
+    int scoreTeamB =0;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+
+    /**
+     * Displays the given scoreTeamA for Team A.
+     */
+    public void displayForTeamA(int score) {
+        TextView scoreView = (TextView) findViewById(R.id.team_a_score);
+        scoreView.setText(String.valueOf(score));
+    }
+
+    public void displayForTeamB(int score) {
+        TextView scoreView = (TextView) findViewById(R.id.team_b_score);
+        scoreView.setText(String.valueOf(score));
+    }
+
+    public void threePoints(View v) {
+        scoreTeamA +=3;
+        displayForTeamA(scoreTeamA);
+    }
+    public void twoPoints(View v) {
+        scoreTeamA +=2;
+        displayForTeamA(scoreTeamA);
+    }
+    public void freeThrow(View v) {
+        scoreTeamA +=1;
+        displayForTeamA(scoreTeamA);
+    }
+
+    public void threePoints2(View v) {
+        scoreTeamB +=3;
+        displayForTeamB(scoreTeamB);
+    }
+    public void twoPoints2(View v) {
+        scoreTeamB +=2;
+        displayForTeamB(scoreTeamB);
+    }
+    public void freeThrow2(View v) {
+        scoreTeamB +=1;
+        displayForTeamB(scoreTeamB);
+    }
+
 }
